2f839ed2d10176a4069db70bf0782e8465ed87b7
Derick <derick_sm@hotmail.com>
2020-01-27 20:24:12 -0300

    first

---
+++ src/test/java/br/com/springmc/springmc/SpringMcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springmc.springmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
